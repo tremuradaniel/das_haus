fbd28e81e8f2fbce2cca2f19340b1db0e0add239
Daniel Tremura <tremuradaniel@gmail.com>
2023-11-25 17:03:05 +0200

    initial symfony commit

---
+++ backend/backend_api/src/Controller/HomePageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomePageController extends AbstractController
+{
+    #[Route('/', name: 'app_home_page')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => sprintf('Welcome to %s API!', $_ENV['APP_NAME'])
+        ]);
+    }
+}
